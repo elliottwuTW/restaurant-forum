250e2a995ae2467ffba91cbd09bc5be8a3bcea7d
elliottwuTW <elliott10009@gmail.com>
2020-11-23 21:46:43 +0800

    Fix: add form validation when user signup

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const session = require('express-session')
+const flash = require('connect-flash')
+
 const db = require('./models')
 const app = express()
 const port = 3000
@@ -9,6 +12,15 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: true }))
+app.use(flash())
+
+// local vars
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- controllers/userController.js
+++ controllers/userController.js
@@ -7,17 +7,33 @@ const signUpPage = (req, res) => {
 }
 
 const signUp = (req, res) => {
-  // hash password
-  bcrypt
-    .genSalt(10)
-    .then((salt) => bcrypt.hash(req.body.password, salt))
-    .then((hash) => {
-      req.body.password = hash
-      User.create(req.body)
-        .then(() => res.redirect('/signin'))
-        .catch((err) => console.error(err))
-    })
-    .catch((err) => console.error(err))
+  // password check
+  if (req.body.password !== req.body.passwordCheck) {
+    req.flash('error_messages', '密碼不同')
+    return res.redirect('/signup')
+  }
+
+  // check user
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (user) {
+      req.flash('error_messages', 'email 已註冊')
+      return res.redirect('/signup')
+    }
+    // hash password
+    bcrypt
+      .genSalt(10)
+      .then((salt) => bcrypt.hash(req.body.password, salt))
+      .then((hash) => {
+        req.body.password = hash
+        User.create(req.body)
+          .then(() => {
+            req.flash('suceess_messages', '註冊成功')
+            return res.redirect('/signin')
+          })
+          .catch((err) => console.error(err))
+      })
+      .catch((err) => console.error(err))
+  })
 }
 
 module.exports = {

447415bbb77daab3734cd2ab058f37574a5cf381
elliottwuTW <elliott10009@gmail.com>
2020-11-25 11:26:28 +0800

    Feat: deploy to heroku

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const methodOverride = require('method-override')
 const db = require('./models')
 const passport = require('./config/passport')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')

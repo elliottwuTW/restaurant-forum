e18379f7088781bce5da58d3e89fd78c496654b1
elliottwuTW <elliott10009@gmail.com>
2020-11-25 11:40:37 +0800

    modify migration add-isAdmin-to-User default value

--- migrations/20201124021604-add-isAdmin-to-User.js
+++ migrations/20201124021604-add-isAdmin-to-User.js
@@ -4,7 +4,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('Users', 'isAdmin', {
       type: Sequelize.BOOLEAN,
-      defaultValue: 0 // no admin by default
+      defaultValue: false // no admin by default
     })
   },
 

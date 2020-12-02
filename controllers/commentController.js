db9c7465175da801a4fa479ce7a8c6daa97472bc
elliottwuTW <elliott10009@gmail.com>
2020-12-02 10:56:38 +0800

    Feat: prevent empty comment

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -2,19 +2,34 @@ const db = require('../models')
 const Comment = db.Comment
 const Restaurant = db.Restaurant
 
+// error handle method
+const {
+  allValidationError,
+  errorMsgToArray
+} = require('../utils/errorHandleHelper')
+
 const postComment = async (req, res) => {
   const restaurant = await Restaurant.findByPk(req.body.restaurantId)
   if (!restaurant) {
     req.flash('error_messages', '餐廳中無此 id')
     return res.redirect('back')
   }
-
-  await Comment.create({
-    text: req.body.text,
-    RestaurantId: restaurant.id,
-    UserId: req.user.id
-  })
-  return res.redirect(`/restaurants/${restaurant.id}`)
+  try {
+    await Comment.create({
+      text: req.body.text,
+      RestaurantId: restaurant.id,
+      UserId: req.user.id
+    })
+    return res.redirect(`/restaurants/${restaurant.id}`)
+  } catch (err) {
+    if (allValidationError(err.errors)) {
+      const validationErrorMsg = errorMsgToArray(err.message)
+      req.flash('error_messages', validationErrorMsg)
+      return res.redirect('back')
+    } else {
+      console.error(err)
+    }
+  }
 }
 
 const deleteComment = async (req, res) => {
